308471c31b78768669dcaa2eff69f8661aac676e
KimDienTruong <kimdientruong1105@gmail.com>
2026-05-11 20:25:05 +0700

    feat: enhance FormModal and InfoModal with close button and border styling

--- frontend/src/components/modals/FormModal.tsx
+++ frontend/src/components/modals/FormModal.tsx
@@ -9,12 +9,25 @@ type FormModalProps = {
   children?: React.ReactNode;
 };
 
-export const FormModal = ({ isOpen, onClose, children, title }: FormModalProps) => {
+export const FormModal = ({
+  isOpen,
+  onClose,
+  children,
+  title,
+}: FormModalProps) => {
   if (!isOpen) return null;
 
   return createPortal(
     <div className="fixed text-black inset-0 h-full flex items-center justify-center z-50 cursor-auto">
-      <div className=" rounded-lg flex flex-col p-6 z-10 w-full max-w-md bg-white h-max">
+      <div className=" rounded-lg border flex flex-col px-6 pt-2 pb-6 z-10 w-full max-w-md bg-white h-max">
+        <div className="flex flex-row-reverse top-4 right-4 z-20 ">
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-offset-1 text-2xl font-bold"
+            onClick={onClose}
+          >
+            &times;
+          </button>
+        </div>
         <div className="flex justify-center">
           <h2 className="text-black text-xl font-bold mb-4 text-center ">
             {title}
--- frontend/src/components/modals/InfoModal.tsx
+++ frontend/src/components/modals/InfoModal.tsx
@@ -13,7 +13,15 @@ const InfoModal = ({ title, message, isOpen, onClose }: InfoModalProps) => {
 
   return createPortal(
     <div className="fixed text-black inset-0 h-full flex items-center justify-center z-50 cursor-auto">
-      <div className=" rounded-lg flex flex-col p-6 z-10 w-full max-w-md bg-white h-max">
+      <div className=" rounded-lg flex flex-col border px-6 pt-2 pb-6 z-10 w-full max-w-md bg-white h-max">
+        <div className="flex flex-row-reverse top-4 right-4 z-20 ">
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-offset-1 text-2xl font-bold"
+            onClick={onClose}
+          >
+            &times;
+          </button>
+        </div>
         <div className="flex justify-center">
           <h2 className="text-black text-xl font-bold mb-4 text-center ">
             {title}
@@ -30,7 +38,7 @@ const InfoModal = ({ title, message, isOpen, onClose }: InfoModalProps) => {
         </div>
       </div>
     </div>,
-    document.getElementById("modal-portal") as HTMLElement
+    document.getElementById("modal-portal") as HTMLElement,
   );
 };
 
